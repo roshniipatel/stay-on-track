422e250a76941a59b819dca91aa03b75343918d4
Roshni Patel <roshnipatel@Roshnis-MacBook-Pro.local>
2023-05-07 16:09:46 -0500

    some starter stuff for js

--- db/schema.sql
+++ db/schema.sql
@@ -8,23 +8,24 @@ CREATE TABLE department (
   name VARCHAR(30) NOT NULL
 );
 
+
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary INT NOT NULL,
   department_id INT NOT NULL,
-  FOREIGN KEY (department_id)
-  REFERENCES department(id)
+  INDEX dep_index (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT NULL,
-  FOREIGN KEY (role_id)
-  REFERENCES role(id),
-  FOREIGN KEY (manager_id) 
-  REFERENCES employee(id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id),
+  INDEX role_index (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
 );
