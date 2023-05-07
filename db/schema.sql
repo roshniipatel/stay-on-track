54829f23f422a3e5c0c34b0632c2a60295b1d415
Roshni Patel <roshnipatel@Roshnis-MacBook-Pro.local>
2023-05-07 16:19:10 -0500

    code modified

--- db/schema.sql
+++ db/schema.sql
@@ -8,24 +8,20 @@ CREATE TABLE department (
   name VARCHAR(30) NOT NULL
 );
 
-
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary INT NOT NULL,
+  salary DECIMAL NOT NULL,
   department_id INT NOT NULL,
-  INDEX dep_index (department_id),
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+  CONSTRAINT sh_department FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
-  manager_id INT NULL,
-  PRIMARY KEY (id),
+  manager_id INT,
   FOREIGN KEY (manager_id) REFERENCES employee(id),
-  INDEX role_index (role_id),
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
+  CONSTRAINT sh_role FOREIGN KEY (role_id) REFERENCES role(id)
 );
