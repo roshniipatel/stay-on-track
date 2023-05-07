912db7a8c532b2124e5c22d63d4ac7bda053e33f
Roshni Patel <roshnipatel@Roshnis-MacBook-Pro.local>
2023-05-07 15:51:35 -0500

    added id's to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,27 +7,25 @@ VALUES
   ('Development'),
   ('Human Resources');
 
-INSERT INTO role (title, salary, department_id) 
+INSERT INTO role (id, title, salary, department_id) 
 VALUES  
-  ('Engineer', 58000, 1),
-  ('Project Manager', 45000, 1),
-  ('Accountant', 70000, 5),
-  ('Product Manager', 17000, 4),
-  ('Recruiter', 42000, 3),
-  ('Team Leader', 54000, 2),
-  ('Manager', 62000, 3),
-  ('Marketing', 32000, 4);
+  (1, 'Engineer', 58000, 1),
+  (2, 'Project Manager', 45000, 1),
+  (3, 'Accountant', 70000, 5),
+  (4, 'Product Manager', 17000, 4),
+  (5, 'Recruiter', 42000, 3),
+  (6, 'Team Leader', 54000, 2),
+  (7, 'Manager', 62000, 3),
+  (8, 'Marketing', 32000, 4);
 
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES 
-  ('Magnus', 'Bane', 9, NULL),
-  ('Alexander', 'Lightwood', 4, NULL),
-  ('Isabella', 'Lightwood', 6, NULL),
-  ('Raphael', 'Santiago', 5, 2),
-  ('Simon', 'Lewis', 1, 2),
-  ('Clarissa', 'Fairchild', 3, 1),
-  ('Jace', 'Herondale', 3, 1),
-  ('Will', 'Herondale', 2, 3),
-  ('Lucian', 'Graymark', 2, 3),
-  ('Jocelyn', 'Fray', 8, NULL);
+  (1, 'Magnus', 'Bane', 9, NULL),
+  (2, 'Alexander', 'Lightwood', 4, NULL),
+  (3, 'Isabella', 'Lightwood', 6, NULL),
+  (4, 'Raphael', 'Santiago', 5, 2),
+  (5, 'Simon', 'Lewis', 1, 2),
+  (6, 'Clarissa', 'Fairchild', 3, NULL),
+  (7, 'Jace', 'Herondale', 3, 1),
+  (8, 'Will', 'Herondale', 2, 3);
