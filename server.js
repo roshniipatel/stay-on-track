2909e5fce2d8deb4fb54098803ca75928459c36f
Roshni Patel <roshnipatel@Roshnis-MacBook-Pro.local>
2023-05-07 21:50:18 -0500

    semi-colon police

--- server.js
+++ server.js
@@ -1,21 +1,3 @@
-// GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
-
 // imports
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
@@ -86,7 +68,7 @@ function init() {
 function Leave() {
   console.log('Toodles!');
   db.exit();
-}
+};
 
 // tutor helped cut and clean up large blocks of codes!
 
@@ -98,7 +80,7 @@ function listDepartment() {
     console.log("Viewing all departments");
     console.table(response);
   })
-}
+};
 
 // function to view all the roles
 function listRoles() {
@@ -108,7 +90,7 @@ function listRoles() {
     console.log("Viewing all the roles");
     console.table(response);
   })
-}
+};
 
 // function for viewing all employees
 function listEmployees() {
@@ -118,7 +100,7 @@ function listEmployees() {
     console.log("Viewing all employees");
     console.table(response);
   })
-}
+};
 
 // function for adding a department 
 function addDepartment() {
@@ -167,7 +149,7 @@ function addRole() {
           console.table(response);
         }
     })
-}
+};
 
 // function for adding a new employee
 function addEmployee() {
@@ -201,8 +183,9 @@ function addEmployee() {
           console.table(response);
         }
     })
-}
+};
 
 // function for updating a current employee
+
 
 init();
